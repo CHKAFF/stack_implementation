2bf7d5aa50c8e4754c380645d81eed634f7e94f5
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-12-12 13:34:00 +0500

    Creat projects

---
+++ Stack/ConsoleStack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Stack;
+namespace ConsoleStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi this is my stack implementation. " +
+                              "You can choose the stack type, for this write one " +
+                              "of the data types (int, float, double, string, bool)." +
+                              "Enter 'exit' to exit");
+            var exitFlag = true;
+            while (exitFlag)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "int":
+                        break;
+                    case "float":
+                        break;
+                    case "double":
+                        break;
+                    case "string":
+                        break;
+                    case "bool":
+                        break;
+                    case "exit":
+                        exitFlag = false;
+                        Console.WriteLine("Goodbye!!!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid data type. Select from the list");
+                        break;
+                }
+            }
+        }
+
+        public bool StackWork<T>()
+        {
+            var exitFlag = true;
+            var stack = new Stack<T>();
+            Console.WriteLine("");
+            while (exitFlag)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "push":
+                        var value = Console.ReadLine();
+                        break;
+                    case "pop":
+                        break;
+                    default:
+                        Console.WriteLine("Enter 'push' or 'pop'");
+                        break;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Stack
+{
+    public class Item<T>
+    {
+        public T Value { get; set; }
+        public Item<T> Next { get; set; }
+        public Item<T> Back { get; set; }
+    }
+
+    public class Stack<T>
+    {
+        private Item<T> head;
+
+        public void Push(T item)
+        {
+            if (head == null)
+            {
+                head = new Item<T> {Value = item, Next = null};
+            }
+            else
+            {
+                var newItem = new Item<T> { Value = item, Next = null, Back = head};
+                head.Next = newItem;
+                head = newItem;
+            }
+        }
+
+        public T Pop()
+        {
+            var result = head.Value;
+            head = head.Back;
+            return result;
+        }
+    }
+}
